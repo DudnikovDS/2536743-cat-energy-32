ec87fbc6c64df8e54dcd1aac74905670dd146548
DudnikovDS <93090336+DudnikovDS@users.noreply.github.com>
2024-09-29 13:31:47 +0300

    Module2-task6 (#10)
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует сетку шапки по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * Фиксирует определенные изменения по заданию 6
    
    * Фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6
    
    * фиксирует определенные изменения по заданию 6

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+const mainNavigationButton = document.querySelector('.main-navigation__button');
+const mainNavigationList = document.querySelector('.main-navigation__list');
+
+mainNavigationButton.onclick = function() {
+  mainNavigationButton.classList.toggle('main-navigation__button--close');
+  mainNavigationList.classList.toggle('main-navigation__list--hidden');
+};
+
