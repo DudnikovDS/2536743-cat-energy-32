dbeeaa1bc9b4105f30449fdcdbc6a484cfbe25fa
DudnikovDS <DudnikovDS@users.noreply.github.com>
2024-10-21 19:17:29 +0000

    Deploying to gh-pages from @ DudnikovDS/2536743-cat-energy-32@5c35512f98fb44097381064aa6eea5e70910b03a 🚀

---
+++ 12/scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-navigation__button"),i=document.querySelector(".main-navigation__list");n.onclick=function(){n.classList.toggle("main-navigation__button--close"),i.classList.toggle("main-navigation__list--hidden")};
